0c3d0ea0afea982fc1fa253aaf029bb852586975
ninjakick75 <jack.artrip@yahoo.com>
2022-09-18 21:43:05 +0800

    Adding base working to set up the connect 4 game

--- connect.py
+++ connect.py
@@ -1,15 +1,31 @@
 # Import connect and other files
 import os
-from connect_helper import Player, string_centre, clear
+from connect_helper import Player, get_name, print_frame, select, clear, color, delay_print
 
 
 # Main function
 def main():
-    print("Hello world")
+
+    # Clear the terminal
+    clear()
+
+    # Print the connect four
+    print_frame(delay=0.01)
+
+    # Ask user for each player's name
+    name1 = get_name(1)
+    print_frame()
+    name2 = get_name(2)
+    print_frame()
+
+    # Ask user to choose their color
+    delay_print(f"{name1}, choose your color:\n")
+    delay_print("1: RED\n", edit=[color.RED])
+    delay_print("2: YELLOW\n", edit=[color.YELLOW])
+    selection = select("Choose: ")
+
 
 
 # Run code
 if __name__ == "__main__":
     main()
-
-
--- connect_helper.py
+++ connect_helper.py
@@ -1,29 +1,100 @@
 # Import packages
 import os
+import sys
+from time import sleep
+
 
 class Player:
 
     # Constructor class
-    def __init__(self, color):
+    def __init__(self, color, name):
+        self.name = name
         self.win = 0
         self.lose = 0
         self.color = color
 
 
 # Print center text
-def string_centre(*strings : str, border="=", sep=" "):
+def string_centre(*strings: str, border="=", sep=" ") -> str:
 
     # Get the length of the terminal
     size = os.get_terminal_size().columns
 
     # Combine the strings
     text = f" {' '.join(strings)} "
-    
+
     # Centre the text
     return text.center(size, border)
 
 
 # Clear the screen
 def clear():
-    os.system('cls' if os.name=='nt' else 'clear')
+    os.system("cls" if os.name == "nt" else "clear")
 
+
+# Delay printing text
+def delay_print(text, delay=0.05, end="\n", edit=[""]):
+
+    # Get length of the edit
+    length = len(edit)
+
+    # Print
+    for i, char in enumerate(text):
+        sys.stdout.write(edit[(i % length) - 1] + char + color.END)
+        sys.stdout.flush()
+        sleep(delay)
+
+    # Print the end argument
+    print(end=end)
+
+
+def print_frame(delay=0) -> None:
+
+    # Clear the terminal
+    clear()
+
+    # Print the logo
+    delay_print(
+        string_centre("Connect Four") + "\n",
+        delay=delay,
+        edit=[color.YELLOW + color.BOLD, color.RED + color.BOLD],
+    )
+
+
+# Get the user name
+def get_name(player: int) -> str:
+    
+    # Get the user input
+    name = input(f"Player {player} name: ")
+
+    # Check if name is nothing
+    if name == "": name = f"Player {player}"
+
+    # Return the name
+    return name.title().strip()
+
+
+# Get the user input of either 1 or 2
+def select(txt):
+    print()
+    while True:
+        try:
+            number = int(input(txt).strip())
+            if number == 1 or number == 2:
+                return number
+        except:
+            print("Usage: '1' or '2'")
+
+
+# Color selection
+class color:
+    PURPLE = "\033[95m"
+    CYAN = "\033[96m"
+    DARKCYAN = "\033[36m"
+    BLUE = "\033[94m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    RED = "\033[91m"
+    UNDERLINE = "\033[4m"
+    BOLD = "\033[1m"
+    END = "\033[0m"
